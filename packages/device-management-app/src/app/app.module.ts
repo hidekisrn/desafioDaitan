94b0569a93f71c60226e83ad1ee0d1cac87876a9
Sergio Nisikava <sergionisikava@gmail.com>
2021-12-29 11:54:47 -0300

    chore: added menu component for management

--- packages/device-management-app/src/app/app.module.ts
+++ packages/device-management-app/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, DevicesComponent, CategoriesComponent],
+  declarations: [AppComponent, DevicesComponent, CategoriesComponent, MenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
